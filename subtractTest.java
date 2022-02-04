72ec416a85fd828782ba03d33fc1909e41dd67d3
xtrasee <thn018@ucsd.edu>
2022-02-03 23:49:13 -0800

    fixed test

--- subtractTest.java
+++ subtractTest.java
@@ -6,6 +6,6 @@ public class subtractTest {
     public void sub() {
         subtract ex = new subtract();
         int ans = 3;
-        assertEquals(ans,ex.sub(7,2)); //failing test
+        assertEquals(ans,ex.sub(5,2)); //failing test
     }
 }
