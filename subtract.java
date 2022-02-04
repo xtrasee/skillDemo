4b6940e86ee9e710e85ff7b909d00aae700614ff
xtrasee <thn018@ucsd.edu>
2022-02-03 23:46:45 -0800

    test

---
+++ subtract.java
@@ -0,0 +1,8 @@
+public class subtract{
+
+    public int sub(int x, int y) { 
+        int total = x - y;  
+        return total;
+    }
+    
+}
---
+++ subtractTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class subtractTest {
+    @Test
+    public void sub() {
+        subtract ex = new subtract();
+        int ans = 3;
+        assertEquals(ans,ex.sub(7,2)); //failing test
+    }
+}
